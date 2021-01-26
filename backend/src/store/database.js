94e580020f8c63ddff1ebff4ecfda5fdf71cdeb8
Lidenbrock-ed <stivenh_1224@hotmail.com>
2021-01-25 23:46:18 -0500

    changed method of connection with the DB

--- backend/src/store/database.js
+++ backend/src/store/database.js
@@ -1,12 +1,20 @@
 const {Sequelize} = require('sequelize');
-const {USER_DEV, PASSWORD_DEV, HOST_DEV, PORTDB, DATABASE_DEV} = process.env;
-const sequelize = new Sequelize(`postgres://${USER_DEV}:${PASSWORD_DEV}@${HOST_DEV}:${PORTDB}/${DATABASE_DEV}`)
+const {USER_DEV, PASSWORD_DEV, HOST_DEV, DATABASE_DEV} = process.env;
+const sequelize = new Sequelize(
+    DATABASE_DEV,
+    USER_DEV,
+    PASSWORD_DEV,
+    {
+        host:HOST_DEV,
+        dialect:'postgres',
+        logging:false,
+})
 async function connect(){
     try{
         sequelize.authenticate();
-        console.info("Connected to the DB successfully")
+        console.log("Connected to the DB successfully")
     }catch(error){
-        console.error("Unexpected error, please resolve this error: ", error);
+        console.error("Unexpected error, please try again");
     }
 };
 connect()
