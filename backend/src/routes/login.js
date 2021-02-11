b193e1eafc7177730103472f7f4c03486cfb0f3f
Lidenbrock-ed <stivenh_1224@hotmail.com>
2021-02-11 18:49:37 -0500

    created function to connect with the controller

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const Router = express.Router();
 const loginUser = require('../controllers/login');
-Router.post('/', loginUser);
+Router.post('/', async function (req,res){
+    let result = await loginUser(req);
+    res.status(result.status).json(result);
+});
 module.exports = Router;
