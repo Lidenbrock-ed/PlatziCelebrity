f1e43012002b66187c697820a4d80ae3e0090d0a
Lidenbrock-ed <stivenh_1224@hotmail.com>
2021-02-18 13:55:02 -0500

    removed function to test

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const Router = express.Router();
-const loginUser = require('../controllers/login');
+const {loginUser}= require('../controllers/login');
 Router.post('/', async function (req,res){
     let result = await loginUser(req);
     res.status(result.status).json(result);
